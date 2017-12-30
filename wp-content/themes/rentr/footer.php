9121e0d75694e9f4e5a319ed7249b8615360cfb2
Britni Fletcher <me@britnidesigns.com>
2017-12-30 13:07:32 -0800

    Basic layout

--- wp-content/themes/rentr/footer.php
+++ wp-content/themes/rentr/footer.php
@@ -14,17 +14,7 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'rentr' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'rentr' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'rentr' ), 'rentr', '<a href="http://underscores.me/">Underscores.me</a>' );
-			?>
-		</div><!-- .site-info -->
+		<p>&copy; 2018 RK Development LC</p>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
--- wp-content/themes/rentr/header.php
+++ wp-content/themes/rentr/header.php
@@ -18,6 +18,7 @@
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 
 	<?php wp_head(); ?>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/common.css">
 </head>
 
 <body <?php body_class(); ?>>
