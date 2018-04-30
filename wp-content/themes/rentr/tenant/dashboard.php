544cefad4fec1d69ec69445707dab22a3e7bf3ff
Britni Fletcher <me@britnidesigns.com>
2018-04-29 20:47:55 -0700

    Fixed display and style of rent due on tenant dashboard, changed body text font

--- wp-content/themes/rentr/tenant/dashboard.php
+++ wp-content/themes/rentr/tenant/dashboard.php
@@ -15,76 +15,51 @@ if ( !empty( $client_ids ) ) {
 }
 
 $amt_due = 0;
+$unpaid_count = 0;
 $rent_output = '';
+$invoice_link = '';
 
-foreach ($invoices as $invoice) {
-    //$invoice_i = SI_Invoice::get_instance( $invoice );
-/*
-[ID] => 306
-[post_author] => 1
-[post_date] => 2018-02-20 12:00:00
-[post_date_gmt] => 2018-02-22 22:14:54
-[post_content] =>
-[post_title] => Recurring Daily
-[post_excerpt] =>
-[post_status] => complete
-[comment_status] => closed
-[ping_status] => closed
-[post_password] =>
-[post_name] => b49702699f951785709df04fcd6dd7dc
-[to_ping] =>
-[pinged] =>
-[post_modified] => 2018-02-22 16:15:03
-[post_modified_gmt] => 2018-02-22 22:15:03
-[post_content_filtered] =>
-[post_parent] => 0
-[guid] => http://localhost/rkdevelopment/pay-rent/b49702699f951785709df04fcd6dd7dc/
-[menu_order] => 0
-[post_type] => sa_invoice
-[post_mime_type] =>
-[comment_count] => 0
-[filter] => raw
-*/
-    $balance = si_get_invoice_balance($invoice);
+foreach ( $invoices as $invoice ) {
+    $balance = si_get_invoice_balance( $invoice );
     $amt_due+= $balance;
 
-    if ($balance > 0) {
-        $due_date = date('M j', si_get_invoice_due_date($invoice));
+    if ( $balance > 0 ) {
+        $unpaid_count++;
 
-        $rent_output.= '<p>
-            $'. $balance .' due '. $due_date .'
-            <a href="'. esc_url( add_query_arg( array( 'dashboard' => 'pay' ), get_permalink( $invoice_id ) ) ) .'">Pay</a>
-        </p>';
+        $due_date = date( 'M j', si_get_invoice_due_date( $invoice ) );
+        $invoice_link = get_permalink( $invoice );
+        //$invoice_link = esc_url( add_query_arg( array( 'dashboard' => 'pay' ), get_permalink( $invoice ) ) );
+
+        $rent_output.= '<div class="invoice">
+            <p><span>$'. $balance .'</span> due '. $due_date .'</p>
+            <a href="'. $invoice_link .'" class="btn-alt">Pay</a>
+        </div>';
     }
-
-    //echo si_get_invoice_status($invoice).' - ';
-    //echo $balance.' - '.$due_date.'<br>';
 }
 
-$due_class = ($amt_due === 0) ? 'paid' : 'unpaid';
+$due_class = ( $amt_due > 0 ) ? 'unpaid' : 'paid';
 
-if ($amt_due === 0) {
-    $rent_output = '<p>✔ All rent has been paid</p>';
-}
+if ( $amt_due <= 0 )
+    $rent_output = '<p><span class="check">✔</span> All rent due has been paid</p>';
 
-else {
-    //TODO: only show $rent_output if more than one invoice, change button link to specifc invoice if only one invoice
+elseif ( $unpaid_count === 1 )
+    $rent_output = '<a href="'. $invoice_link .'" class="btn">Pay Rent</a>';
 
-    $rent_output.= '<a href="'. get_site_url() .'/rent" class="btn">Pay Rent</a>'; // TODO: add invoice url
-}
+else
+    $rent_output.= '<a href="'. get_site_url() .'/rent" class="btn">Pay Rent</a>';
 ?>
 
 <div class="dashboard">
 	<section>
 		<h2>Rent Due</h2>
-        <p class="balance <?=$due_class?>">$<?=$amt_due?></p>
+        <p class="balance <?=$due_class?>"><sup>$</sup><?=number_format( $amt_due, 2 )?></p>
 		<?=$rent_output?>
-		<a href="<?=get_site_url()?>/rent">Rent History</a>
+		<a href="<?=get_site_url()?>/rent" class="history">Rent History</a>
 	</section>
 	<section>
 		<h2>Maintenance</h2>
 		<p>TODO: Messages overview</p>
 		<a href="<?=wpas_get_submission_page_url(); ?>" class="btn">New Request</a>
-		<a href="<?=get_site_url()?>/maintenance">Maintenance History</a>
+		<a href="<?=get_site_url()?>/maintenance" class="history">Maintenance History</a>
 	</section>
 </div>
