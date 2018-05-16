8f4a222ebcedb097bc1e3d47175a562f43412d64
Britni Fletcher <me@britnidesigns.com>
2018-05-15 19:41:02 -0700

    Added Font Awesome CDN

--- wp-content/themes/rentr/header.php
+++ wp-content/themes/rentr/header.php
@@ -17,6 +17,7 @@
 
 	<?php wp_head(); ?>
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/common.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js" integrity="sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe" crossorigin="anonymous"></script>
 </head>
 
 <body <?php body_class(); ?>>
